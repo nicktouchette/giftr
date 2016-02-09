b97875c65f787c6d9da146d2276dd8c8f3539521
Chad Christensen <chadthecoder@gmail.com>
2016-02-09 07:58:26 -0500

    Almost done with animation on home screen

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  // Show first event on home page
+  $('.events li').first().show();
+
+  // Home page animation
+  var $events = $('.events li');
+  var $currentEvent = $events.first();
+  var animationOut = 'fadeOutUp';
+  var animationIn = 'fadeInUp';
+  var $lastEvent = $events.last();
+
+  function fadeUpInterval () {
+    var animateInterval = setInterval(fadeNext, 2000);
+  }
+
+  function fadeNext() {
+
+    $($currentEvent).prev().removeClass(animationIn);
+    $($currentEvent).addClass('animated');
+    $($currentEvent).addClass(animationOut);
+    $($currentEvent).next().addClass('animated');
+    $($currentEvent).next().addClass(animationIn);
+
+    if ($currentEvent.text() == $lastEvent.text()) {
+      $currentEvent = $events.first();
+      $currentEvent.removeClass(animationOut).addClass(animationIn);
+    } else {
+      $currentEvent = $($currentEvent).next();
+      $($currentEvent).removeClass(animationOut);
+    }
+
+  }
+
+  fadeUpInterval();
+
+  for(var i = 0; i < $events.length; i++) {
+      // $($events[i]).delay(1).show('slow');
+    }
+});
